4c625002d9b6151fbf9f68e73065ccee47727172
utkan <utkan131@hotmail.com>
2022-09-10 17:00:45 +0300

    Changing for deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -11,8 +12,14 @@ app.use(bodyParser.json());
 
 const indexRouter = require('./routes/index.js');
 
+
 app.use(cors());
 app.use("/", indexRouter);
+
+app.use(express.static(path.join(__dirname,'/views/build')));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/build", "index.html"));
+});
 
 app.listen(process.env.PORT || 5000)
 
